17db24f1d04f747a261a432e59126bd76f9a9c41
Nikola Totev <nikola.r.totev@gmail.com>
2019-05-16 12:04:06 +0300

    Completed writing image test

--- Course Project/Image_Creation_Test/RGB.h
+++ Course Project/Image_Creation_Test/RGB.h
@@ -9,9 +9,11 @@ struct RGB
 		G = g;
 		B = b;
 
-		
+		pixelData[0] = r;
+		pixelData[1] = g;
+		pixelData[2] = b;
 	}
-
+	unsigned char pixelData[3];
 	unsigned char  R;
 	unsigned char G;
 	unsigned char B;
