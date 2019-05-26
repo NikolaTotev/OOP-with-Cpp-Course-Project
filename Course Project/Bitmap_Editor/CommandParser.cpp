4f4ba126addb600f30be2da5ca407dc8ecf5fa68
Nikola Totev <nikola.r.totev@gmail.com>
2019-05-26 14:16:27 +0300

    Partially completed command parser.

--- Course Project/Bitmap_Editor/CommandParser.cpp
+++ Course Project/Bitmap_Editor/CommandParser.cpp
@@ -10,3 +10,79 @@ CommandParser::CommandParser()
 CommandParser::~CommandParser()
 {
 }
+
+void CommandParser::separate_input(std::string input)
+{
+	input += " ";
+	char space = ' ';
+	char dash = '-';
+	int dashCounter=0;
+	for (int i = 0; i < input.length(); ++i)
+	{
+		switch (currentState) {
+		case seek_cmnd:
+
+			if (input.at(i) == dash)
+			{
+				dashCounter += 1;
+				if(dashCounter == 2)
+				{
+				currentState = read_cmd;
+				dashCounter = 0;
+				}
+			}
+			break;
+		case read_cmd:
+			if(input.at(i) != space)
+			{
+				if(input.at(i)!= dash)
+				{				
+					temp_cmnd += input.at(i);
+				}
+				
+			}
+			else
+			{
+				if(!valid_command())
+				{
+					cout << "Invalid command" << endl;
+					break;								
+				}
+				currentState = read_path;
+			}
+			break;
+		case seek_path:
+			break;
+		case read_path:			
+			if (input.at(i) != space)
+			{
+				temp_path += input.at(i);
+			}
+			else
+ 			{	
+				if(!temp_path.empty())
+				{
+					filePaths.push_back(temp_path);
+					temp_path = "";
+					if(i != input.size())
+					{
+						currentState = seek_cmnd;
+					}
+				}
+				
+			}
+			break;
+		default:;
+		}
+	}
+}
+
+bool CommandParser::valid_command()
+{
+	if(temp_cmnd == "tmpCmd" || temp_cmnd =="other")
+	{
+		commands.push_back(temp_cmnd);
+		temp_cmnd = "";
+		return true;
+	}
+}
--- Course Project/Bitmap_Editor/CommandParser.h
+++ Course Project/Bitmap_Editor/CommandParser.h
@@ -1,8 +1,21 @@
 #pragma once
+#include  <string>
+#include <vector>
+#include <iostream>
+using namespace std;
 class CommandParser
 {
+	enum  states {seek_cmnd, seek_path, read_cmd, read_path};
+	states currentState = seek_cmnd;
 public:
 	CommandParser();
 	~CommandParser();
+	std::vector<std::string> commands;
+	std::vector<std::string> filePaths;
+	std::string command_start = "-";
+	string temp_cmnd="";
+	string temp_path="";
+	void separate_input(std::string input);
+	bool valid_command();
 };
 
